cf81cd5e2c016ff9d8a972a64879f4f8b0893b6a
joshuatolen <jtolen@me.com>
2024-12-25 13:38:39 -0500

    added missing import

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from PIL import Image, ImageFilter
 from moviepy import VideoFileClip
 
