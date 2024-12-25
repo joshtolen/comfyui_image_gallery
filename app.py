dbc0831677207b8b45dca3c08a7ae36b2b306ddf
joshuatolen <jtolen@me.com>
2024-12-25 11:33:46 -0500

    Fixed image_dir -> file_dir

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def image_gallery():
     # Get a list of image files from the specified directory
     image_files = [f for f in os.listdir(file_dir) if f.endswith(('.jpg', '.png', '.jpeg', '.gif', '.bmp', '.webp', '.mp4', '.avi', '.mov', '.mkv', '.webm', '.mp5'))]
     # Sort the list of image files by last modified date (newest first)
-    image_files.sort(key=lambda x: os.path.getmtime(os.path.join(image_dir, x)), reverse=True)
+    image_files.sort(key=lambda x: os.path.getmtime(os.path.join(file_dir, x)), reverse=True)
 
     # Get the page number from the query string (default to 1)
     page = int(request.args.get('page', 1))
